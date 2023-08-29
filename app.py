2c7c57a0fe37615753324264f01aae343ec015a4
Gabriel E Kreindler <Gek@mit.edu>
2023-08-28 21:27:43 -0400

    messages based on job availability

--- app.py
+++ app.py
@@ -25,6 +25,29 @@ def isInt(s):
     except ValueError:
         return False
 
+def find_polygon_id(latitude, longitude, nbhd_gdf):
+    point = gpd.GeoSeries([Point(longitude, latitude)])
+    point_in_polygons = nbhd_gdf.contains(point.geometry[0])
+
+    if any(point_in_polygons):
+        polygon_names = nbhd_gdf.loc[point_in_polygons, 'name']
+        polygon_ids = nbhd_gdf.loc[point_in_polygons, 'id']
+
+        job_available = bool(1 - nbhd_gdf.loc[point_in_polygons, 'excluded'].min())
+
+        print("returing:")
+        print(polygon_names.iloc[0])
+        print(polygon_ids.iloc[0])
+        print("job available", job_available, " base on ", list(nbhd_gdf.loc[point_in_polygons, 'excluded']))
+
+        return polygon_names.iloc[0], int(polygon_ids.iloc[0]), job_available
+
+        # if len(polygon_names) > 1:
+        #     return "-2"  # in multiple polygons, should never happen
+        # else:
+        #     return polygon_names.iloc[0]
+    else:
+        return "-99", "-99", False  # not in any of the polygons
 
 class GeoFence(Resource):
     def get(self):
@@ -38,7 +61,7 @@ class GeoFence(Resource):
         print("###xxx###:GEOFENCE. time=TIME,devid={},loginid={},lat={},lon={}".format(deviceid, loginid, latitude,
                                                                                        longitude))
 
-        if isNumeric(latitude) and isNumeric(longitude) and isNumeric(loginid):
+        if isNumeric(latitude) and isNumeric(longitude) and isInt(loginid):
             latitude = float(latitude)
             longitude = float(longitude)
             loginid = int(loginid)
@@ -59,42 +82,26 @@ class GeoFence(Resource):
 
             nbhds_gdf = nbhds_gdf.merge(jobs_df, on='id')
 
-            polygon_name, polygon_id, job_available = self.find_polygon_id(latitude, longitude, nbhds_gdf)
+            polygon_name, polygon_id, job_available = find_polygon_id(latitude, longitude, nbhds_gdf)
 
             if polygon_name == "-99":
-                return {"success": False, "message": "couldn't find in any grid"}, 200
+                return {
+                           "success": False,
+                           "message": "Cannot find neighborhood. Please move to a new location and try again."
+                       }, 200
 
             elif polygon_name == "-2":
                 return {"success": False, "message": "found in multiple polygons"}, 200
 
-            return {"success": True, "message": "in neighborhood name=" + str(polygon_name) + ", and id=" + str(polygon_id)}, 200
+            if job_available:
+                message_job = "the task IS available here."
+            else:
+                message_job = "the task IS NOT available here."
+
+            return {"success": True, "message": "You are in neighborhood " + str(polygon_name) + ", " + message_job}, 200
         
         else:
             return {"success": False, "message": "Bad request, latitude and longitude should be numeric parameter"}, 400
-
-    def find_polygon_id(self, latitude, longitude, nbhd_gdf):
-        point = gpd.GeoSeries([Point(longitude, latitude)])
-        point_in_polygons = nbhd_gdf.contains(point.geometry[0])
-
-        if any(point_in_polygons):
-            polygon_names = nbhd_gdf.loc[point_in_polygons, 'name']
-            polygon_ids = nbhd_gdf.loc[point_in_polygons, 'id']
-
-            job_available = bool(1 - nbhd_gdf.loc[point_in_polygons, 'excluded'].min())
-
-            print("returing:")
-            print(polygon_names.iloc[0])
-            print(polygon_ids.iloc[0])
-            print("job available", job_available, " base on ", list(nbhd_gdf.loc[point_in_polygons, 'excluded']))
-
-            return polygon_names.iloc[0], int(polygon_ids.iloc[0]), job_available
-
-            # if len(polygon_names) > 1:
-            #     return "-2"  # in multiple polygons, should never happen
-            # else:
-            #     return polygon_names.iloc[0]
-        else:
-            return "-99", "-99", False  # not in any of the polygons
 
 
 class Login(Resource):
@@ -149,16 +156,41 @@ class StartTask(Resource):
         # logging
         print("###xxx###:START_TASK. time=TIME,devid={},loginid={},lat={},lon={}".format(deviceid, loginid, latitude,
                                                                                           longitude))
-        ru = random.random()
-        if isInt(loginid) and isNumeric(latitude) and isNumeric(longitude) and ru < 0.5:
-            loginid = int(loginid)
 
+        if isInt(loginid) and isNumeric(latitude) and isNumeric(longitude):
+            loginid = int(loginid)
             latitude = float(latitude)
             longitude = float(longitude)
 
-            return {"success": True, "message": "Start Task OK"}, 200
+            # Read the shapefile
+            shapefile_path = 'shapefile_nairobi_L1/nairobi_L1.shp'
+            nbhds_gdf = gpd.read_file(shapefile_path)
+
+            # read job offers
+            jobs_path = 'tables/pilot8_unstructured_excluded.csv'
+            jobs_df = pd.read_csv(jobs_path)
+            jobs_df = jobs_df[jobs_df.uniqueid == loginid]
+            jobs_df.rename(columns={'nbh_code': 'id'}, inplace=True)
+
+            nbhds_gdf = nbhds_gdf.merge(jobs_df, on='id')
+
+            polygon_name, polygon_id, job_available = find_polygon_id(latitude, longitude, nbhds_gdf)
+
+            if polygon_name == "-99":
+                return {
+                           "success": False,
+                           "message": "Cannot find neighborhood. Please move to a new location and try again."
+                       }, 200
+
+            elif polygon_name == "-2":
+                return {"success": False, "message": "found in multiple polygons"}, 200
+
+            if job_available:
+                return {"success": True, "message": "Start Task OK"}, 200
+            else:
+                return {"success": True, "message": "Task not available here in neighborhood " + polygon_name}, 200
         else:
-            return {"success": False, "message": "Login or deviceid not integer or lat or long not float, or arguments not provided"}, 400
+            return {"success": False, "message": "Error."}, 400
 
 
 class StopTask(Resource):
